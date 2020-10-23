9d089b412c02092439038f2b8af991efa1f71590
rauldiaazz <raul.diazb@estudiante.uam.es>
2020-10-23 12:37:19 -0400

    ordenacion bueno

--- ordenacion.c
+++ ordenacion.c
@@ -60,6 +60,57 @@ int InsertSortInv(int *tabla, int ip, int iu)
 
 
 
+short Combinar(int *t, int p, int m, int u){
+    int i, j, k;
+    int *aux;
+    assert(i >= 0);
+    assert(t);
+    aux = (int *)malloc((u - p + 1) * sizeof(aux[0]));
+    if (!aux)
+        return ERR;
+
+    i = p;
+    j = m + 1;
+    k = p;
+    while (i <= m && j <= u)
+    {
+        if (t[i] < t[j])
+        {
+            aux[k] = t[i];
+            i++;
+        }
+        else
+        {
+            aux[k] = t[j];
+            j++;
+            k++;
+        }
+    }
+    if (i > m) 
+        while (j <= u)
+        {
+            aux[k] = t[j];
+            j++;
+            k++;
+        }
+    else if (j > u)
+    {                                             
+        while (i < m)
+        {
+            aux[k] = t[i];
+            i++;
+            k++;
+        }
+    for (i = p, j = 0; i < u; i++, j++)
+        {
+            t[i] = aux[j];
+        }
+        free(aux);
+        return OK;
+    }
+
+
+
 
 int mergesort(int* tabla, int ip, int iu){
     int cont=0;
